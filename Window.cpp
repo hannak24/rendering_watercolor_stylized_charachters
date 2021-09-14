a733b9058cf411969438da131091f84728fc8431
hannak24 <73960535+hannak24@users.noreply.github.com>
2021-09-15 01:42:19 +0300

    adjust camera movements

--- Window.cpp
+++ Window.cpp
@@ -309,18 +309,22 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
+        camera.ProcessKeyboard(UP, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
+        camera.ProcessKeyboard(DOWN, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
 
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        modelPitchAngle = modelPitchAngle + 0.7;
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
         modelPitchAngle = modelPitchAngle - 0.7;
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        modelPitchAngle = modelPitchAngle + 0.7;
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
         modelYawAngle = modelYawAngle + 0.7;
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
