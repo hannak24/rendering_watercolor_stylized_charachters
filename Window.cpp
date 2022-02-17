c329a39a917371500678e69ee49641de401c1755
hannak24 <73960535+hannak24@users.noreply.github.com>
2022-02-17 02:07:45 +0200

    minimizing bug fixed

--- Window.cpp
+++ Window.cpp
@@ -752,6 +752,9 @@ int main()
         glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shader.use();
+        if (SCR_HEIGHT == 0) { //if window is minimized
+            SCR_HEIGHT = 1;
+        }
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         shader.setMat4("projection", projection);
